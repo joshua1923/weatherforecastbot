ebbfc51d5200dfa037a19f1367144dba43a48eb4
joshua1923 <34540377+joshua1923@users.noreply.github.com>
2018-06-26 17:02:57 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,7 @@
 import json
 import os
-import requests
+import urllib
+import sys
 
 from flask import Flask
 from flask import request
@@ -11,39 +12,13 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    print(json.dumps(req, indent=4))
-    
-    res = processRequest(req)
-    
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeResponse(req):
-    if req.get("result").get("action") != "fetchWeatherForecast":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    date = parameters.get("date")
-    if city is None:
-        return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b507f3d778d195799fc96f68a50f9d25')
-    json_object = r.json()
-    weather=json_object['list']
-    for i in range(0,30):
-        if date in weather[i]['dt_txt']:
-            condition= weather[i]['weather'][0]['description']
-            break
-    speech = "The forecast for"+city+ "for "+date+" is "+condition
-    return {
-    "fulfillmentText": speech,
-    "FulfillmentText": speech,
-    "source": "apiai-weather-webhook"
-    }
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = "select wind from weather.forecast where woeid=2460286"
+    yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+    result = urllib2.urlopen(yql_url).read()
+    data = json.loads(result)
+    print data['query']['results']
+        
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
