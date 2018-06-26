a6fb133961c2c0779faf03f658679341594d0de3
jlindsay <jlindsay@rydon.co.uk>
2018-06-26 13:58:29 +0100

    changed code

--- webhook.py
+++ webhook.py
@@ -13,33 +13,41 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
-
-    res = makeResponse(req)
-    res = json.dumps(req, indent=4)
+    
+    res = processRequest(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeResponse(req):
-    result = req.get('result')
-    parameters = result.get('parameters')
-    city = parameters.get('geo-city')
-    date = parameters.get('date')
-    r = requests.get('http://http://api.openweathermap.org/data/2.5/forecast?q='+city+',us&appid=b507f3d778d195799fc96f68a50f9d25)
+    if req.get("result").get("action") != "fetchWeatherForecast":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    date = parameters.get("date")
+    if city is None:
+        return None
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b507f3d778d195799fc96f68a50f9d25')
     json_object = r.json()
-    weather = json_object['list']
+    weather=json_object['list']
     for i in range(0,30):
         if date in weather[i]['dt_txt']:
-            condition = weather[i]['weather'][0]['description']
+            condition= weather[i]['weather'][0]['description']
             break
-    speech = 'The forecast for' + city + 'for' + date + 'is' + condition
+    speech = "The forecast for"+city+ "for "+date+" is "+condition
     return {
-        'speech': speech,
-        'displayText': speech,
-        'source': 'apiai-weather-webhook'
+    "speech": speech,
+    "displayText": speech,
+    "source": "apiai-weather-webhook"
     }
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', '5000'))
-    print('Starting app on port %d' % port)
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
+
+
